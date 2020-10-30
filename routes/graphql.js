da4500e8d42200cf3736fd59077296f86619d607
abdurahmonbek <abdurahmonkodirov97@gmail.com>
2020-10-30 11:48:09 +0500

    example with an IP

--- routes/graphql.js
+++ routes/graphql.js
@@ -5,44 +5,25 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
-
   type Query {
-    getDie(numSides: Int): RandomDie
+    ip: String
   }
 `);
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
-  }
+const loggingMiddleware = (req, res, next) => {
+  console.log('ip:', req.ip);
+  next();
 }
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  getDie: ({numSides}) => {
-    return new RandomDie(numSides || 6);
+  ip: function (args, request) {
+    return request.ip;
   }
 };
 
 /* Graphql page. */
+router.use(loggingMiddleware);
 router.use('/', graphqlHTTP({
   schema: schema,
   rootValue: root,
