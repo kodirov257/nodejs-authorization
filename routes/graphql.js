8642628c5d860d5d1e184f397fc8eca8cffc0e9c
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2021-12-13 14:40:14 +0500

    Added example with object types

--- routes/graphql.js
+++ routes/graphql.js
@@ -5,14 +5,40 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+  
   type Query {
     hello: String
     quoteOfTheDay: String
     random: Float!
     rollThreeDice: [Int]
     rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
+
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
 
 // The root provides a resolver function for each API endpoint
 var root = {
@@ -35,6 +61,9 @@ var root = {
     }
     return output;
   },
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
+  }
 };
 
 /* Graphql page. */
